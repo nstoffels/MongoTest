f4bfc80c85ec3208136d4aab807b6b945b3f4116
nstoffels <n.stoffels@groupe-esigelec.fr>
2015-07-03 14:39:17 +0200

    début de l'étape 2

--- bankonet-conseiller/src/main/java/ConseillerMain.java
+++ bankonet-conseiller/src/main/java/ConseillerMain.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import org.bson.Document;
+
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
@@ -33,10 +36,19 @@ public class ConseillerMain {
 		
 		//Création d'un client avec MongoClient
 		MongoClient mongoClient = new MongoClient();
+		//récupère la database de Mongo
+		MongoDatabase client = mongoClient.getDatabase("persondb");
+		//récupération dans la collection (table) de mongo les objets persons
+		MongoCollection<Document> clients = client.getCollection("persons");
+		//récupère la database de Mongo
+		MongoDatabase comptecourant = mongoClient.getDatabase("persondb");
+		//récupération dans la collection (table) de mongo les objets comptes
+		MongoCollection<Document> cc = comptecourant.getCollection("persons");
 		do{
 			System.out.println("Bienvenue opérateur");
 			System.out.println("0. Arrêter le programme");
 			System.out.println("1. ouvrir un compte");
+			System.out.println("2. lister tout les clients");
 			System.out.println("sélectionner un choix");
 			key=sc.nextInt();
 
@@ -52,14 +64,7 @@ public class ConseillerMain {
 				case 1:
 				   System.out.println("création du client");
 				   // TODO Auto-generated method stub					
-					//récupère la database de Mongo
-					MongoDatabase client = mongoClient.getDatabase("persondb");
-					//récupération dans la collection (table) de mongo les objets persons
-					MongoCollection<Document> clients = client.getCollection("persons");
-					//récupère la database de Mongo
-					MongoDatabase comptecourant = mongoClient.getDatabase("persondb");
-					//récupération dans la collection (table) de mongo les objets comptes
-					MongoCollection<Document> cc = comptecourant.getCollection("persons");
+					
 					
 					
 					//initialisation des variables
@@ -92,8 +97,33 @@ public class ConseillerMain {
 					cc.insertOne(compte);
 					List<Document> docs=new ArrayList<Document>();
 					doc.append("comptescourant", docs);
+					
 			break;
 			
+			
+			case 2:
+				MongoCollection<Document> clients1 = client.getCollection("persons");
+				MongoCollection<Document> cc1 = comptecourant.getCollection("persons");
+				//liste de tout les documents de la collection
+				//définition des itérators
+				Object name, forname, log, ID;
+				Iterator<Document> clientelle = clients1.find().iterator();
+				Iterator<Document> comptelecture = cc1.find().iterator();
+				//affichage des différentes informations client dans la console
+				while(clientelle.hasNext()){
+					
+					Document listclient = clientelle.next();
+					name = listclient.get("nom");
+					forname = listclient.get("prenom");
+					log = listclient.get("login");
+					ID = listclient.get("_id");
+					System.out.println("log: "+log+" ID: "+ID+" nom: "+name+" prenom: "+forname);
+				}
+				
+//				while(comptelecture.hasNext()){
+//					System.out.println(comptelecture.next());
+//				}
+			break;
 
 			default:
 			   System.out.println("recommencer");
